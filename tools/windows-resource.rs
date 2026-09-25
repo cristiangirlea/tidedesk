51fea0daa3a828885d4950e45e3129473dcaf609
Cristian Girlea <cristiangirlea@gmail.com>
2026-09-25 19:20:50 +0300

    Fix the resource definition's escape (build script)

--- tools/windows-resource.rs
+++ tools/windows-resource.rs
@@ -36,7 +36,7 @@ pub fn embed() {
         format!("TIDEDESK_BINARY=\"{binary}\\0\""),
         format!("TIDEDESK_FILENAME=\"{binary}.exe\\0\""),
         // Task Manager, firewall and UAC prompts show this as the program's name.
-        "TIDEDESK_DESCRIPTION=\"TideDesk\0\"".to_string(),
+        "TIDEDESK_DESCRIPTION=\"TideDesk\\0\"".to_string(),
     ];
     embed_resource::compile("../../assets/tidedesk.rc", &definitions)
         .manifest_optional()
